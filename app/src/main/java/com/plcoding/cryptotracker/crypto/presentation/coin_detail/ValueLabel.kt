6a8c4083c69addd14f01e7bfa98d2bff8b1551b8
Jonathan Jensen <jpjensen@utah.gov>
2024-10-21 15:18:03 -0600

    Finish up coin detail screen and start on getting coin history.

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/ValueLabel.kt
@@ -0,0 +1,18 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_detail
+
+import android.icu.text.NumberFormat
+import java.util.Locale
+
+data class ValueLabel(
+    val value: Float,
+    val unit: String
+) {
+    fun formatted(): String {
+        val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+            val fractionDigits = when {
+                value > 1000 -> 0
+                value in 2f .. 999f -> 2
+            }
+        }
+    }
+}
