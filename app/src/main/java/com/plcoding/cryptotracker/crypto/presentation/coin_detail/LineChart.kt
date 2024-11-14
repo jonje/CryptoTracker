e3e279d70c1574be193be82b796d5cae22b3b347
Jonathan Jensen <haiyun211@gmail.com>
2024-11-14 12:13:18 -0700

    [WIP] Building Line Chart

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/LineChart.kt
@@ -1,9 +1,20 @@
 package com.plcoding.cryptotracker.crypto.presentation.coin_detail
 
+import android.graphics.Color
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun LineChart(
@@ -29,4 +40,68 @@ fun LineChart(
         visibleDataPoints.maxOfOrNull { it.y } ?: 0f
     }
 
+    val minYValue = remember (visibleDataPoints) {
+        visibleDataPoints.minOfOrNull { it.y } ?: 0f
+    }
+
+    val measurer = rememberTextMeasurer()
+
+    var xLabelWidth by remember {
+        mutableFloatStateOf(0f)
+    }
+
+    LaunchedEffect(key1 = xLabelWidth) {
+        onXLabelWidthChange(xLabelWidth)
+    }
+
+    val selectedDataPointIndex = remember (selectedDataPoint) {
+        dataPoints.indexOf(selectedDataPoint)
+    }
+
+    var drawPoints by remember {
+        mutableStateOf(listOf<DataPoint>())
+    }
+
+    var isShowingDataPoints by remember {
+        mutableStateOf(selectedDataPoint != null)
+    }
+
+    Canvas(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        val minLabelSpacingYPx = style.minYLabelSpacing.roundToPx()
+        val verticalPaddingPx = style.verticalPadding.roundToPx()
+        val horizontalPaddingPx = style.horizontalPadding.roundToPx()
+        val xAxisLabelSpacingPx = style.xAxisLabelSpacing.roundToPx()
+
+        val xLabelTextLayoutResults = visibleDataPoints.map {
+            measurer.measure(
+                text = it.xLabel,
+                style = textStyle.copy(textAlign = TextAlign.Center)
+            )
+        }
+
+        val maxXLabelWidth = xLabelTextLayoutResults.maxOfOrNull { it.size.width } ?: 0
+        val maxXLabelHeight = xLabelTextLayoutResults.maxOfOrNull { it.size.height } ?: 0
+        val maxXLabelLineCount = xLabelTextLayoutResults.maxOfOrNull { it.lineCount } ?: 0
+        val xLabelLineHeight = maxXLabelHeight / maxXLabelLineCount
+
+        val viewPortHeightPx = size.height - (maxXLabelHeight + 2 * verticalPaddingPx + xAxisLabelSpacingPx)
+        val viewPortTopY = verticalPaddingPx + xLabelLineHeight + 10f
+        val viewPortRightX = size.width
+        val viewPortBottomY = viewPortTopY + viewPortRightX
+        val viewPortLeftX = 2f * horizontalPaddingPx
+        val viewPort = Rect(
+            left = viewPortLeftX,
+            top = viewPortTopY,
+            right = viewPortRightX,
+            bottom = viewPortBottomY
+        )
+
+        drawRect(
+            color = Color.green
+        )
+
+    }
 }
